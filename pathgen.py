aa33b75377e2abf20dacec7acef3ecaf44b2c544
Arun Lakshmanan <c.arun.lakshmanan@gmail.com>
2015-02-25 10:53:46 -0600

    Added major and minor axes

--- pathgen.py
+++ pathgen.py
@@ -16,6 +16,10 @@ rob_wheel_x = 0.04 * scale_img * rob_scale
 rob_wheel_y = 0.02 * scale_img * rob_scale
 rob_wheel_pos = 2.0/3.0 #Value between 1 and 0
 
+#Grid Size
+grid_space = 0.5 
+dot_space = 0.05 
+
 UDP = "localhost"
 PORT = 20000
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
@@ -23,8 +27,8 @@ sock.bind((UDP,PORT))
 
 print("\n\nPress ctrl + C to exit\n\n")
 
-pix_x = int(room_width*scale_img)
-pix_y = int(room_length*scale_img)
+pix_x = int(room_width * scale_img)
+pix_y = int(room_length * scale_img)
 
 color_bot = {1: color(71,82,224),
 			 2: color(0,255,30),
@@ -43,6 +47,8 @@ def draw():
 	fill(255,255,255)
 	rectMode(CORNER)
 	rect(0.0,0.0,width,height)
+	draw_major_axes()
+	draw_minor_axes()
 	udprecv, addr = sock.recvfrom(1024)
 	worldx,worldy,yaw,rbid,status,dataRate = struct.unpack('<dddddd',udprecv)
 	x,y = convert_coord(worldx,worldy)
@@ -60,6 +66,30 @@ def draw():
 		rotate(row[2])
 		draw_robot(row[3])
 		popMatrix()
+
+def draw_major_axes():
+	stroke(0)
+	line(pix_x/2,0,pix_x/2,pix_y)
+	line(0,pix_y/2,pix_x,pix_y/2)
+
+def draw_minor_axes():
+	noSmooth()
+	for i in range(int(room_width/(2*grid_space))):
+		for j in range(int(room_length/(2*dot_space))):
+			draw_point(convert_coord(i*grid_space,j*dot_space))
+			draw_point(convert_coord(-i*grid_space,j*dot_space))
+			draw_point(convert_coord(i*grid_space,-j*dot_space))
+			draw_point(convert_coord(-i*grid_space,-j*dot_space))
+	for j in range(int(room_length/(2*grid_space))):
+		for i in range(int(room_width/(2*dot_space))):
+			draw_point(convert_coord(i*dot_space,j*grid_space))
+			draw_point(convert_coord(-i*dot_space,j*grid_space))
+			draw_point(convert_coord(i*dot_space,-j*grid_space))
+			draw_point(convert_coord(-i*dot_space,-j*grid_space))
+
+
+def draw_point(pt):
+	point(pt[0],pt[1])
 
 def convert_coord(mocapx,mocapy):
 	px = pix_x/2 + (mocapx*scale_img)

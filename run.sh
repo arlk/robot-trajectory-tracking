ff4797c868769fbeff52bb591493c534ce5c8104
Arun Lakshmanan <c.arun.lakshmanan@gmail.com>
2015-02-25 09:16:29 -0600

    Adding PATH variable functionality

--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-#Address of your processing-py.sh script
-process=/home/arun/Documents/processing/processing.py-0202-linux64/processing-py.sh
-sampleproc=/home/arun/sketchbook/mynew/mynew.py
-udpsend=/home/arun/sketchbook/mynew/udpsend.py
-udprecv=/home/arun/sketchbook/mynew/udprecv.py
+# Before executing add the directory of processing-py.sh and, 
+# pathgen_processing folder to your PATH environment variable
+process=processing-py.sh
+pathgen=pathgen.py
+udpsend=udps.py
 
 echo "UDP simulation data sending..."
 exec ${udpsend} &
 
 echo "UDP receiving..."
 echo "Starting simulation..."
-exec ${sampleproc}
+${process} ${pathgen}
+
+# Use the following cmd if program crashes 
+# to stop udps.py to send data continously:
+# kill -9 `ps -ef | grep python | grep -v grep | awk '{print $2}'`

9e07f8b3c331c0321abaeddc723f6f6b77f2cc8a
Arun Lakshmanan <c.arun.lakshmanan@gmail.com>
2015-02-25 06:16:59 -0600

    Adding independence from nbots

--- pathgen.py
+++ pathgen.py
@@ -2,30 +2,57 @@ import socket
 import struct
 
 #Room size
-length = 5.5
-width = 3.35
-scale_img = 20
+room_length = 5.5
+room_width = 3.35
+scale_img = 120.0
+
+#Robot dimensions
+rob_x = int(0.15 * scale_img)
+rob_y = int(0.2 * scale_img)
 
 UDP = "localhost"
-PORT = 10000
+PORT = 20000
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind((UDP,PORT))
 
-print("\n\nPress ctrl + C to exit")
+print("\n\nPress ctrl + C to exit\n\n")
 
-pix_x = int(width*scale_img)
-pix_y = int(length*scale_img)
+pix_x = int(room_width*scale_img)
+pix_y = int(room_length*scale_img)
+
+color_bot = {1: color(58,116,197),
+			 2: color(0,255,30),
+			 3: color(220,20,60),
+			 4: color(240,255,0)}
+
+data = []
 
 def setup():
 	size(pix_x,pix_y)
-	colorMode(HSB)
+	colorMode(RGB)
 	noStroke()
+	frameRate(1)
 
 def draw():
-	data, addr = sock.recvfrom(1024)
-	x,y,yaw,rbid,size = struct.unpack('<ddddd',data)
-	fill(0x11000000)
-	rect(0,0,width,height)
-	fill(int(85*rbid),255,255)
-	rotate(yaw)
-	rect(x,y,20,20)
+	fill(255,255,255)
+	rectMode(CORNER)
+	rect(0.0,0.0,width,height)
+	udprecv, addr = sock.recvfrom(1024)
+	x,y,yaw,rbid,status = struct.unpack('<ddddd',udprecv)
+	rbid = int(rbid)
+	if status == 1:
+		col = [row[3] for row in data]
+		if (rbid in col) != 1:
+			data.append([x,y,yaw,rbid])
+		else:
+			data[col.index(rbid)] = [x,y,yaw,rbid]
+	for row in data:
+		fill(color_bot[row[3]])
+		translate(row[0],row[1])
+		rotate(row[2])
+		rectMode(CENTER)
+		rect(0,0,rob_x,rob_y)
+		translate(-row[0],-row[1])
+		rotate(-row[2])
+
+
--- udpsend.py
+++ udpsend.py
@@ -4,17 +4,24 @@ import socket
 import struct
 
 UDP = "localhost"
-PORT = 10000
-x = 10
-y = 20
-yaw = 1
-rbid = 1
-status = 1
+PORT = 20000
+x = 10.0
+y = 20.0
+yaw = 0.0
+rbid = 1.0
+status = 1.0
 nbot = 3
 
-
-while True:
-	for i in range(1,4):
-		sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-		msg = struct.pack('ddddd',x+i*100,y+i*100,yaw+i,rbid+i,status)
-		sock.sendto(msg, (UDP,PORT))
+try:
+	while True:
+		for i in range(1,nbot+1):
+			sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+			xs = x + 100*i
+			ys = y + 100*i
+			rbid = i
+			yaw = i*0.5
+			msg = struct.pack('<ddddd',xs,ys,yaw,rbid,status)
+			sock.sendto(msg, (UDP,PORT))
+except:
+	print("\nClosing Ports\n")
+	sock.close()

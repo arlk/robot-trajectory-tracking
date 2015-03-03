5d3c99d14557c5f5b2dc3dea0fccdfaa9ae5ca41
Arun Lakshmanan <c.arun.lakshmanan@gmail.com>
2015-03-03 00:59:06 -0600

    minor fixes

--- udps.py
+++ udps.py
@@ -7,7 +7,7 @@ import struct
 import math
 
 UDP = "localhost"
-PORT = 20000
+PORT = 10000
 xs = [1.0, -1.0,  1.0]
 ys = [1.0, -1.0, -1.0]
 yaw = [1.0, -0.5, -3.141]
@@ -19,7 +19,7 @@ try:
 	while True:
 		for i in range(0,nbots):
 			sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-			msg = struct.pack('<dddddd',xs[i],ys[i],yaw[i],i+1,status,count)
+			msg = struct.pack('<dddddd',xs[i],ys[i],yaw[i],i+2,status,count)
 			sock.sendto(msg, (UDP,PORT))
 			count += 1
 
